cf7aa44d54b84a19f41a3090c6743a30e40a85cf
Trifusz Huba <huba.trifusz@gmail.com>
2024-10-17 19:27:51 +0200

    header component done

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../button/button.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, HeaderComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
