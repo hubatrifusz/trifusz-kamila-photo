cc5f7ca659a620084964eebd2176d6c0a817f789
Trifusz Huba <huba.trifusz@gmail.com>
2024-10-17 20:13:50 +0200

    line component added

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from "../button/button.component";
 import { HeaderComponent } from "../header/header.component";
 import { ParagraphComponent } from '../paragraph/paragraph.component';
+import { LineComponent } from "../line/line.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [ButtonComponent, HeaderComponent, ParagraphComponent],
+  imports: [ButtonComponent, HeaderComponent, ParagraphComponent, LineComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
